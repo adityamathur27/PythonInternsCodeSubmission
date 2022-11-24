bb69fef4ee3ae7e2bc2c8542d4f681c3df6cf08b
Sureshpaswan0015 <76004761+Sureshpaswan0015@users.noreply.github.com>
2022-11-24 18:58:46 +0530

    Add files via upload

---
+++ Suresh Paswan/fibonacci.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def fib(n):
+    a, b = 0, 1
+    while a <n:
+        print(a, end= ", ")
+        a, b = b, a+b
+        print()
+
+
+# In[ ]:
+
+
+
+
